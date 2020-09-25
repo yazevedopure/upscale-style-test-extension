b357495f164d04c229e82f0050ed43047da95b98
matteo pini <matteo.pini@horsa.it>
2020-09-25 17:20:15 +0200

    Postinstall

--- scripts/install.js
+++ scripts/install.js
@@ -1,12 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log('install');
 const src_path = path.join(__dirname, '../', 'src');
 const root_path = path.join(__dirname, '../');
 
 const component_copy = (cmp) => {
-    fs.mkdirSync(path.join(root_path, cmp))
+
+    const cmp_path = path.join(root_path, cmp);
+    if (!fs.existsSync(cmp_path)){
+        fs.mkdirSync(cmp_path);
+    }
+
+    // copio tutti i file all'interno del componente
+    fs.readdirSync(path.join(src_path, cmp)).forEach(f => {
+
+        if (path.extname(f) === '.scss') {
+            scss_file = fs.readFileSync(
+                path.join(src_path, cmp, f)
+            ).toString();
+            scss_file = scss_file.replace('@import \'../styles/style\';', scss_vars);
+            fs.writeFileSync(
+                path.join(cmp_path, f),
+                scss_file
+            )
+        }
+    })
 }
 
 //  contenuto di _variables.scss
